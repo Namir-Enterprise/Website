21876872e3abf5fa4dc645af8d16482866706d50
Oleksandr <alekceenkosanek@gmail.com>
2022-11-06 19:37:51 +0200

    Add removing dropdown by clicking on site

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,8 +1,16 @@
-const menuLink = document.querySelector('.lang-btn');
+const btnLink = document.querySelector('.lang-btn');
 const subMenu = document.querySelector('.lang__submenu');
 const arrowBtn = document.querySelector('.lang-btn__arrow');
-menuLink.addEventListener('click', event => {
+btnLink.addEventListener('click', event => {
   event.preventDefault();
   subMenu.classList.toggle('open');
   arrowBtn.classList.toggle('active');
 });
+
+document.addEventListener('click', event => {
+  const withinBoundaries = event.composedPath().includes(btnLink);
+  if (!withinBoundaries) {
+    subMenu.classList.remove('open');
+    arrowBtn.classList.remove('active');
+  }
+})
