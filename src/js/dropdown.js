c604ffabf21e5f04ad2ec055b77bdd0a7ecb276d
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-11-07 15:44:52 +0200

    Main Fix: anim. text in header, add blur imgs

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,16 +1,21 @@
 const btnLink = document.querySelector('.lang-btn');
 const subMenu = document.querySelector('.lang__submenu');
 const arrowBtn = document.querySelector('.lang-btn__arrow');
-btnLink.addEventListener('click', event => {
+btnLink.addEventListener('click', (event) => {
   event.preventDefault();
   subMenu.classList.toggle('open');
   arrowBtn.classList.toggle('active');
 });
 
-document.addEventListener('click', event => {
+document.addEventListener('scroll', () => {
+  subMenu.classList.remove('open');
+  arrowBtn.classList.remove('active');
+});
+
+document.addEventListener('click', (event) => {
   const withinBoundaries = event.composedPath().includes(btnLink);
   if (!withinBoundaries) {
     subMenu.classList.remove('open');
     arrowBtn.classList.remove('active');
   }
-})
+});
--- src/js/header-scroll.js
+++ src/js/header-scroll.js
@@ -1,24 +1,23 @@
 const header = document.querySelector('.header');
-const first = document.querySelector('.banner')
+const first = document.querySelector('.banner');
 const headerHeight = header.offsetHeight; //variable to add margin for first element after header
-const containHide = () => header.classList.contains("header__hide")
+const containHide = () => header.classList.contains('header__hide');
 let lastScrollTop = 0;
-first.style.paddingTop = `${headerHeight}px`;;
+first.style.paddingTop = `${headerHeight}px`;
 
-window.addEventListener('scroll', () => {
-   let scrollDistance = window.scrollY;
-   if (scrollDistance < lastScrollTop && containHide()) {
-      header.classList.remove('header__hide');
-   }
+document.addEventListener('scroll', handleScrollHeader);
 
-   else if (scrollDistance > lastScrollTop && !containHide()) {
-      header.classList.add('header__hide');
-   }
+function handleScrollHeader() {
+  let scrollDistance = window.scrollY;
+  if (scrollDistance < lastScrollTop && containHide()) {
+    header.classList.remove('header__hide');
+  } else if (scrollDistance > lastScrollTop && !containHide()) {
+    header.classList.add('header__hide');
+  }
 
-   if (scrollDistance === 0) {
-      header.classList.remove('header__hide');
-   }
+  if (scrollDistance === 0) {
+    header.classList.remove('header__hide');
+  }
 
-   lastScrollTop = scrollDistance;
-});
-
+  lastScrollTop = scrollDistance;
+}
