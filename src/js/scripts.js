cf5ea534a4046286ccf4604dc33ff3a037883e51
Volodymyr Fouchek <vinvova555333@gmail.com>
2022-11-08 22:37:47 +0200

    Add: proto of tabs with animation without logic

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,7 @@
 // Custom Scripts
-@@include('dropdown.js');
+@@include('dropdown.js')
 @@include('burger-menu.js')
 @@include('header-scroll.js')
+@@include('tabs_test.js') //need to delete
+@@include('tabs.js')
+@@include('indicator.js')
