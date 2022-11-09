8f178e2ea60181d58a2e3259b13bd833979029ef
Oleksandr <alekceenkosanek@gmail.com>
2022-11-09 03:29:56 +0200

    Fix: accordion

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,16 +1,21 @@
-const accordions = document.querySelectorAll('.accordion-item');
-accordions.forEach(el => {
-   el.addEventListener('click', (e) => {
-      const self = e.currentTarget;
-      const content = self.querySelector('.accordion-item__content');
-      self.classList.toggle('open');
-
-      if (self.classList.contains('open')) {
-         content.style.maxHeight = content.scrollHeight + 'px';
-         content.style.opacity = 1;
-      }
-      else {
-         content.style.maxHeight = null;
-      }
-   });
-});
+// Аккордеон
+function accordion() {
+   const items = document.querySelectorAll('.accordion-item__control')
+   items.forEach(item => {
+      item.addEventListener('click', () => {
+           const parent = item.parentNode
+           const content = parent.querySelector('.accordion-item__content');
+         if (parent.classList.contains('open')) {
+            parent.classList.remove('open')
+            content.style.maxHeight = null;
+         } else {
+            document.querySelectorAll('.accordion-item').forEach(child => child.classList.remove('open'))   
+            document.querySelectorAll('.accordion-item__content').forEach(cum => cum.style.maxHeight = null)  
+            parent.classList.add('open')
+            content.style.maxHeight = content.scrollHeight + 'px';
+            content.style.opacity = 1;
+         }
+       })
+   })
+}
+accordion() 
