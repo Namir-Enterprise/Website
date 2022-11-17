d72aad8176e25bad62e09d85a1a83eeea47cb20c
Oleksandr <alekceenkosanek@gmail.com>
2022-11-17 17:05:26 +0200

    Burger fix

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -3,6 +3,7 @@ function burgerMenu() {
   const burger = document.querySelector('.burger');
   const menu = document.querySelector('.menu');
   const body = document.querySelector('body');
+  const menuLink = document.querySelectorAll('.menu__item-link')
 
   burger.addEventListener('click', () => {
     if (!menu.classList.contains('active')) {
@@ -24,5 +25,14 @@ function burgerMenu() {
       body.classList.remove('locked');
     }
   });
+
+  menuLink.forEach((e) => {
+    e.addEventListener('click', () => {
+      menu.classList.remove('active');
+      burger.classList.remove('active-burger');
+      body.classList.remove('locked');
+    }
+  )});
 }
 burgerMenu();
+
